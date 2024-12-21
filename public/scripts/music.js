3c7d9f62fe832c9c346d7d436ec7a576aa4af403
Peyman-Sabzizade <peyman.front.developer@gmail.com>
2024-12-21 13:32:09 +0330

    adding dark/light mode

--- public/scripts/music.js
+++ public/scripts/music.js
@@ -1,15 +1,33 @@
 let $ = document
 let usernameValue = $.querySelector(".username")
+let modeBtn = $.querySelector(".mode")
+let darkIcon = $.querySelector(".dark-icon")
+let lightIcon = $.querySelector(".light-icon")
+let modeFlag = true
+localStorage.setItem("mode", "light")
 const logoutBtn = $.querySelector(".log-out")
-const getUsername = localStorage.getItem("Username")
+const getUsername = localStorage.getItem("username")
 function setUsername (username) {
     usernameValue.innerHTML = username
 }
-function removeItem () {
-    localStorage.removeItem("Username")
-}
 setUsername(getUsername)
+function removeItem () {
+    localStorage.removeItem("username")
+}
 logoutBtn.addEventListener("click", function () {
     removeItem()
     window.location.href = "./index.html"
+})
+modeBtn.addEventListener("click", function () {
+    if (modeFlag) {
+        darkIcon.classList.toggle("hidden")
+        lightIcon.classList.toggle("hidden")
+        modeFlag = false
+        localStorage.setItem("mode", "dark")
+    }else {
+        darkIcon.classList.toggle("hidden")
+        lightIcon.classList.toggle("hidden")
+        modeFlag = true
+        localStorage.setItem("mode", "light")
+    }
 })
